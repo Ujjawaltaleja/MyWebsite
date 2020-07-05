ad70840f7b5aa05159fb9720ce095ccbcd5ca58d
Ujjawaltaleja <ujjawaltaleja@gmail.com>
2020-07-05 22:34:45 +0530

    expected commit

---
+++ precache-manifest.131e151aec1a101f82156fe457936fe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b8258e68645103ebd694e9b598f065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce319b995e6a1e5e672f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bba9b0654592d0072db0",
+    "url": "/static/js/2.88f35308.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88f35308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce319b995e6a1e5e672f",
+    "url": "/static/js/main.694a91ed.chunk.js"
+  },
+  {
+    "revision": "064652e579a9570f9ab6",
+    "url": "/static/js/runtime-main.30f34e61.js"
+  }
+]);
